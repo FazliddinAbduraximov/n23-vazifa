4ffb8f7197404e4d8fef2745357fb617948630e9
Fazliddin <fazliddin.abdurahimov9602@gmail.com>
2025-08-27 22:48:00 +0500

    vaifa

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(String(process.env.MONGO_URI)),
+    UserModule,
+    PostModule,
+  ],
+  
+})
+export class AppModule {}
