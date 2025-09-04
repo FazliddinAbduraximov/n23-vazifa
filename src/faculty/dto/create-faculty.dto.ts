4dbf0f0f27fbf8edb8d5dbebc67487b7e7ef6799
Fazliddin <fazliddin.abdurahimov9602@gmail.com>
2025-09-04 18:39:32 +0500

    Serverga

--- src/faculty/dto/create-faculty.dto.ts
+++ src/faculty/dto/create-faculty.dto.ts
@@ -1,10 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateFacultyDto {
+    @ApiProperty({
+        example:"Yuridiskiy"
+    })
     @IsString()
     @IsNotEmpty()
     name:string;
 
+    @ApiProperty({example:1})
     @IsNumber()
     @IsNotEmpty()
     unversityId:number;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
     forbidNonWhitelisted:true,
     errorHttpStatusCode:HttpStatus.UNPROCESSABLE_ENTITY
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   const PORT=Number(process.env.PORT) || 4000
   await app.listen(PORT,()=>console.log('Server run in port ',PORT));
 }
